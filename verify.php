0039fe61d4763f8e35043c5b6f447d52244a5853
calypso-bots <39587472+calypso-bots@users.noreply.github.com>
2018-08-06 15:27:26 +0700

    UPDATE
    UPDATE

--- verify.php
+++ verify.php
@@ -5,9 +5,9 @@
   $link = pg_Connect("host=ec2-23-23-216-40.compute-1.amazonaws.com port=5432 dbname=deko3n85j4ri1q user=fiaknamxlunoil password=d100670ed9d12fe4e7607538cdc26be62bb487aa71db488d59bc045a3169e9b1");
   $result = pg_query($link, "SELECT status from public.check_ok where check_result = 'OPT';");
   if ($_GET["check"] = "OK") {
-	  $put = pg_query($link, "UPDATE public.check_ok SET where status = 'OK' where check_result = 'OPT';");
+	  $put = pg_query($link, "UPDATE public.check_ok SET status = 'OK' where check_result = 'OPT';");
   }elseif ($_GET["check"] = "FAILED") {
-	  $put = pg_query($link, "UPDATE public.check_ok SET where status = 'FAILED' where check_result = 'OPT';");
+	  $put = pg_query($link, "UPDATE public.check_ok SET status = 'FAILED' where check_result = 'OPT';");
   }
   $numrows = pg_numrows($result);
   while ($row = pg_fetch_row($result)) {
