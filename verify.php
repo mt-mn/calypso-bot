a225ee4401d3994eab40fe1827d9262e6e71a026
calypso-bots <39587472+calypso-bots@users.noreply.github.com>
2018-08-06 17:03:55 +0700

    dd
    ss

--- verify.php
+++ verify.php
@@ -1,6 +1,3 @@
-<html>
- <body>
-
 <?php
   $link = pg_Connect("host=ec2-23-23-216-40.compute-1.amazonaws.com port=5432 dbname=deko3n85j4ri1q user=fiaknamxlunoil password=d100670ed9d12fe4e7607538cdc26be62bb487aa71db488d59bc045a3169e9b1");
   $check_status=$_GET["check"];
@@ -19,7 +16,4 @@
 	echo "<br />\n";
 }
 	pg_close($link);
-?><br>
-
- </body>
- </html> 
+?>
