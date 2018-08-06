df484e012ed1956f591aaf4474366ea0bd17a94e
calypso-bots <39587472+calypso-bots@users.noreply.github.com>
2018-08-06 15:06:17 +0700

    cfd
    ss

--- verify.php
+++ verify.php
@@ -3,7 +3,7 @@
 
 <?php echo $_GET["check"]; 
 	$link = pg_Connect("host=ec2-23-23-216-40.compute-1.amazonaws.com port=5432 dbname=deko3n85j4ri1q user=fiaknamxlunoil password=d100670ed9d12fe4e7607538cdc26be62bb487aa71db488d59bc045a3169e9b1");
-  $result = pg_exec($link, "SELECT status from public.check_ok where check_result = 'OPT';");
+  $result = pg_query($link, "SELECT status from public.check_ok where check_result = 'OPT';");
   $numrows = pg_numrows($result);
   echo "<p>link = $link<br>
   result = $result<br>
