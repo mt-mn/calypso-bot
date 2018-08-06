1abaaa4e060e91af2adece432ff954accf24a4b1
calypso-bots <39587472+calypso-bots@users.noreply.github.com>
2018-08-06 15:08:09 +0700

    dd
    ss

--- verify.php
+++ verify.php
@@ -5,10 +5,11 @@
 	$link = pg_Connect("host=ec2-23-23-216-40.compute-1.amazonaws.com port=5432 dbname=deko3n85j4ri1q user=fiaknamxlunoil password=d100670ed9d12fe4e7607538cdc26be62bb487aa71db488d59bc045a3169e9b1");
   $result = pg_query($link, "SELECT status from public.check_ok where check_result = 'OPT';");
   $numrows = pg_numrows($result);
-  echo "<p>link = $link<br>
-  result = $result<br>
-  numrows = $numrows</p>
-  ";
+  while ($row = pg_fetch_row($result)) {
+	echo "value1: $row[0]  value2: $row[1]";
+	echo "<br />\n";
+}
+
 ?><br>
 
  </body>
