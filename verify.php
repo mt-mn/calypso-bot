247e1f93ac4fb11183d9ecb5390b059c8d146ee4
calypso-bots <39587472+calypso-bots@users.noreply.github.com>
2018-08-06 16:19:16 +0700

    ddd
    sss

--- verify.php
+++ verify.php
@@ -5,11 +5,11 @@
   $link = pg_Connect("host=ec2-23-23-216-40.compute-1.amazonaws.com port=5432 dbname=deko3n85j4ri1q user=fiaknamxlunoil password=d100670ed9d12fe4e7607538cdc26be62bb487aa71db488d59bc045a3169e9b1");
   if (!$_GET["check"]) {
 	  if ($_GET["check"] = "OK") {
-		  $put = pg_exec($link, "UPDATE public.check_ok SET status = 'OK' where check_result = 'OPT';");
+		  $put = pg_query($link, "UPDATE public.check_ok SET status = 'OK' where check_result = 'OPT';");
 			echo "OK $put";
 	  }
 	  elseif ($_GET["check"] = "FAIL") {
-		  $put = pg_exec($link, "UPDATE public.check_ok SET status = 'FAIL' where check_result = 'OPT';");
+		  $put = pg_query($link, "UPDATE public.check_ok SET status = 'FAIL' where check_result = 'OPT';");
 		  echo "FAIL $put";
 	  }
 	  pg_query("COMMIT");
