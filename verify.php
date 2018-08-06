f691c213b03587c46ec55f3c4d43ce4d6c61742e
calypso-bots <39587472+calypso-bots@users.noreply.github.com>
2018-08-06 10:48:26 +0700

    verify
    verify

--- verify.php
+++ verify.php
@@ -1,16 +1,7 @@
-<?php
-$access_token = 'URw2lqQ/3WOSnKjaeshjmPKaebXyUpXyenAH3oCjzfrhLiqpYdEJCjmTkeLrw0g9pw+SMfGTH8DoFkk4yaHrs8j5aiYjsTXuinktjKgFOzPvKr5eXr1lldg0O7RT2OBOjnRRNb+1teHEbdTgyOOyYQdB04t89/1O/w1cDnyilFU=';
+<html>
+ <body>
 
+<?php echo $_POST["check"]; ?><br>
 
-$url = 'https://api.line.me/v1/oauth/verify';
-
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
+ </body>
+ </html> 
